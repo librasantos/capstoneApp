cab76e3b8f7e4869117fdb57d0d86d6a0356d8a1
Libra <ly.santos@gmail.com>
2018-02-13 21:36:12 -0500

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,41 @@
+// Select color input
+
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+  var width=$('#inputWidth').val();
+  var column=$('#inputHeight').val();
+  var table =$("#pixelCanvas");
+  for (var h=1; h<=column; h++){
+     var row= $('<tr></tr>');
+     for (var w=1; w<=width; w++){
+       row.append('<td></td>');
+}
+  table.append(row);
+}
+}
+
+$("#sizePicker" ).submit(function( event ) {
+  event.preventDefault();
+  makeGrid();
+
+  $("td").click(function() {
+    var colorNew=$('#colorPicker').val();
+    //alert( "Handler for .click() called." );
+    $(this).css( "background", colorNew );
+  });
+
+  $("td").mouseover(function() {
+    var colorNew=$('#colorPicker').val();
+    //alert( "Handler for .click() called." );
+    $(this).css( "background", colorNew );
+  });
+
+  $( "#my-reset" ).click(function() {
+    //alert( "Handler for .click() called." );
+    $("#pixelCanvas tr").remove();
+  });
+
+});
